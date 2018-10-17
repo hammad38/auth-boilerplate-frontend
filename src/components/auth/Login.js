c82d313a1ff543909c6a3d2f46597082d440b46c
Sayyed Hammad Ali <hammad_ali447@yahoo.com>
2018-10-17 10:36:05 +0200

    login form created

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -4,7 +4,34 @@ class Login extends Component {
   render() {
     return (
       <div>
-        <h1>Login</h1>
+        <div className="card mt-4">
+          <div className="card-header">
+            <h1 className="text-center"><i className="fas fa-lock"/> Login</h1>
+          </div>
+          <div className="card-body">
+            <form>
+              <div className="form-group">
+                <label htmlFor="email"><i className="fas fa-envelope"/> Email</label>
+                <input
+                  className="form-control"
+                  type="email"
+                  placeholder="Enter email"
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="password"><i className="fas fa-key"/> Password</label>
+                <input
+                  className="form-control"
+                  type="password"
+                  placeholder="Password"
+                />
+              </div>
+
+              <button type="submit" className="btn btn-success float-right">Login</button>
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
